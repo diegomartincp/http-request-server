faa095350358589e2b43ab7139cff78836d38e04
Diego Martín <campos.martin.diego@gmail.com>
2023-02-07 10:51:29 +0100

    basic http server

---
+++ api.py
@@ -0,0 +1,10 @@
+from flask import Flask,jsonify
+import flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    response = flask.jsonify({'some': 'data'})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
